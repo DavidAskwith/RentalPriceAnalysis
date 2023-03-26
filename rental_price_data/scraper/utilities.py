171b280ed39330f63f3f8ff9f129470c16bb76e1
David Askwith <dkaskwith@hotmail.com>
2023-03-25 21:03:07 -0400

    Create utilities.py

---
+++ rental_price_data/scraper/utilities.py
@@ -0,0 +1,9 @@
+from selenium import webdriver
+
+def get_web_driver(headless=True):
+    options = webdriver.ChromeOptions()
+    options.add_argument('--ignore-certificate-errors')
+    options.add_argument('--incognito')
+    if headless:
+        options.add_argument('--headless')
+    return webdriver.Chrome(chrome_options=options)
